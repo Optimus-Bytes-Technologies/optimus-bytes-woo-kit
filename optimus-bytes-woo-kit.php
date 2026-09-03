4fea70b0e6554ee69057fc89ebf23565bc5e1b72
Kuppuraj <kuppuraj@live.in>
2026-09-03 18:35:28 +0530

    chore: bump version to 1.3.1 and add background styling properties to filter and swatch elements

--- optimus-bytes-woo-kit.php
+++ optimus-bytes-woo-kit.php
@@ -21,7 +21,7 @@ namespace OptimusBytes\WooKit;
 defined('ABSPATH') || exit;
 
 // Plugin Constants
-define('OBWK_VERSION', '1.3.0');
+define('OBWK_VERSION', '1.3.1');
 define('OBWK_PLUGIN_FILE', __FILE__);
 define('OBWK_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('OBWK_PLUGIN_URL', plugin_dir_url(__FILE__));
